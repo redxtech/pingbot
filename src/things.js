786e18604af38f979fe41cb4c00def08babd383e
Gabe Dunn <gabed@hey.com>
2021-09-19 11:37:57 -0600

    add some things

--- src/things.js
+++ src/things.js
@@ -1,14 +1,35 @@
+const { generateNickname } = require('./utils')
+
 module.exports = {
   things: [
     {
-      "name": "Ping",
-      "probability": 10,
-      "exec": m => m.channel.send("@everyone")
+      name: "Ping",
+      probability: 10,
+      exec: m => m.channel.send("@everyone")
     },
     {
-      "name": "React",
-      "probability": 5,
-      "exec": m => m.react(m.guild.emojis.cache.random())
+      name: "React",
+      probability: 5,
+      exec: m => m.react(m.guild.emojis.cache.random())
+    },
+    {
+      name: "Nickname",
+      probability: 5,
+      exec: m => m.member.setNickname(generateNickname(), 'you just got pingbotted')
+        .then()
+        .catch(err => console.error(`Couldn't change ${m.member.user.tag}'s nickname: ${err}`))
+    },
+    {
+      name: "PingBot Love",
+      probability: 1,
+      exec: m => /\bi\b.+\b(love|like|appreciate)\b.+\bpingbot\b/.test(m.content.toLowerCase())
+        && m.reply('heart <3')
+    },
+    {
+      name: "PingBot Hate",
+      probability: 1,
+      exec: m => /(\bi\b.+\b(hate|dislike)\b.+\bpingbot\b)|(\bpingbot\b.+\b(sucks|is (bad|garbage|trash|ass|shit))\b)|(fuck.+pingbot)/.test(m.content.toLowerCase())
+        && m.reply(':\'(')
     }
   ]
 }
--- src/utils.js
+++ src/utils.js
@@ -6,6 +6,27 @@ const random = upTo => (Math.floor(Math.random() * upTo) + 1)
 // probability tester
 const should = outOf => random(outOf) === 1
 
+// generate a random string of characters to use as a nickname
+const generateNickname = () => {
+  // initialize empty array to store the name in
+  const nicknameArray = []
+
+  // a list of possible characters to show up in the nickname
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-=_+[]\{}|;\':",./<>? '
+
+  // the length of the nickname, between 6 & 26
+  const nicknameLength = 6 + random(20)
+
+  // push a random character from the characters into the nickname array <nicknameLength> times
+  for (let i = 0; i < nicknameLength; i++) {
+    nicknameArray.push(characters.charAt(random(characters.length)))
+  }
+
+  // return the nickname as a string
+  return nicknameArray.join('')
+}
+
 module.exports = {
-  should 
+  should,
+  generateNickname
 }
