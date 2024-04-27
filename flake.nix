856802978b9265fb7f32c5b7696de97586c8182f
Gabe Dunn <gabe@sent.at>
2024-04-27 05:47:58 -0700

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,70 @@
+{
+  description = "Pingbot";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    devenv.url = "github:cachix/devenv";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-schemas.url =
+      "https://flakehub.com/f/DeterminateSystems/flake-schemas/*.tar.gz";
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys =
+      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = inputs@{ nixpkgs, flake-parts, devenv, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      # systems to enable support for
+      systems =
+        [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" "aarch64-linux" ];
+
+      imports = [ inputs.devenv.flakeModule ];
+
+      flake.schemas = inputs.flake-schemas.schemas;
+
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        devenv.shells.default = {
+          name = "pingbot";
+
+          packages = with pkgs; [
+            git
+            nixpkgs-fmt
+
+            kubectl
+            kustomize
+            kubeseal
+
+            autoconf
+            automake
+            libtool
+          ];
+
+          # languages to enable
+          languages = {
+            javascript = {
+              enable = true;
+              package = pkgs.nodejs_22;
+              corepack.enable = true;
+            };
+            typescript.enable = true;
+          };
+
+          # dotenv.enable = true;
+
+          # scripts that will be available in the shell
+          scripts.build.exec = "yarn build";
+          scripts.dev.exec = "yarn dev";
+          scripts.run.exec = "yarn start";
+          scripts.seal.exec = "kubeseal --format=yaml < ci/_secret.yaml > ci/secret.yaml";
+          scripts.deploy.exec = "kubectl apply -k ci && kubectl rollout restart deployment/pingbot";
+
+          # processes to run with `devenv up`
+          processes.web.exec = "yarn start";
+        };
+      };
+    };
+}
+
